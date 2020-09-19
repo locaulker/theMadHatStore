9f32823d9084f178b6ff585320edee0100780c68
Lawrence <Caulker>
2020-09-18 22:44:17 -0400

    added Product Template

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,8 +1,9 @@
-import styled from "styled-components"
+import styled from 'styled-components';
+import './styles';
 
 export const LayoutWrapper = styled.div`
   margin: 0 auto;
   max-width: 1200px;
   box-sizing: border-box;
   padding: 0 20px 40px 20px;
-`
+`;

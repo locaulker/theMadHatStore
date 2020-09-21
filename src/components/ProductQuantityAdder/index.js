e4e7d4d3128b459c247095c610c44aef58d09aaf
Lawrence <locaulker@gmail.com>
2020-09-20 22:24:53 -0400

    added product quantity adder functionality

--- src/components/ProductQuantityAdder/index.js
+++ src/components/ProductQuantityAdder/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../Button';
 import { Input } from '../Input';
 import { ProductQuantityAdderWrapper } from './styles';
 
 export function ProductQuantityAdder({ variantId, available }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = e => {
+    setQuantity(e.currentTarget.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <ProductQuantityAdderWrapper>
       <strong>Quantity</strong>
-      <form>
-        <Input />
-        <Button fullWidth>Add To Cart</Button>
+      <form onSubmit={handleSubmit}>
+        <Input
+          disabled={!available}
+          type="number"
+          min="1"
+          step="1"
+          value={quantity}
+          onChange={handleQuantityChange}
+        />
+        <Button type="submit" disabled={!available} fullWidth>
+          Add To Cart
+        </Button>
       </form>
     </ProductQuantityAdderWrapper>
   );

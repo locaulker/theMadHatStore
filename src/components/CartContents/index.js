2f8e36d352e281fb84ebf2e357cca59bc9ad9d4e
Lawrence <Caulker>
2020-09-21 15:38:46 -0400

    completed cart page

--- src/components/CartContents/index.js
+++ src/components/CartContents/index.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import CartContext from 'context/CartContext';
 import { CartItem, CartHeader, CartFooter } from './styles';
 import { QuantityAdjuster } from '../QuantityAdjuster';
+import { RemoveLineItem } from '../RemoveLineItem';
 
 export function CartContents() {
   const { checkout, updateLineItem } = useContext(CartContext);
@@ -35,6 +36,9 @@ export function CartContents() {
             <QuantityAdjuster item={item} onAdjust={handleAdjustQuantity} />
           </div>
           <div>${(item.quantity * item.variant.price).toFixed(2)}</div>
+          <div>
+            <RemoveLineItem lineItemId={item.id} />
+          </div>
         </CartItem>
       ))}
       <CartFooter>
---
+++ src/components/RemoveLineItem/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Icon = styled.div`
+  cursor: pointer;
+  color: red;
+
+  &:hover {
+    color: maroon;
+  }
+`;

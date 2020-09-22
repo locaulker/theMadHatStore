b432875ca6fb8669bdc8524dbdf8425ae468af48
Lawrence <locaulker@gmail.com>
2020-09-22 00:56:37 -0400

    added shopify collections, refactored graphql queries as fragments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Layout, SEO } from 'components';
+import ProductContext from 'context/ProductContext';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to the Gatsby &amp; Shopify starter.</p>
-  </Layout>
-);
+const IndexPage = () => {
+  const { collections } = useContext(ProductContext);
+  console.log(collections);
+
+  return <Layout></Layout>;
+};
 
 export default IndexPage;
